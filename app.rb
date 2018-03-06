9b75fcf1a52c8b7629ecc52bb2376f0853d9b03a
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-03-06 19:13:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,46 +2,48 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/reversename/:name' do
-    "#{params[:name].reverse!}"
-  end
-  
-  get '/square/:number' do
-    answer = params[:number].to_i * params[:number].to_i
-    return answer.to_s
-  end
-  
-  get '/say/:number/:phrase' do
-    counter = params[:number].to_i
-    string = ''
-    while counter > 0
-      string = string params[:phrase]
-      counter = counter - 1
+    get '/reversename/:name' do
+      @reversename = params[:name].reverse
+      "#{@reversename}"
     end
-    return string
-  end
-  
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-    string = ''
-    string = string params[:word1] " "
-    string = string params[:word2] " "
-    string = string params[:word3] " "
-    string = string params[:word4] " "
-    string = string params[:word5] "."
-  end
-  
-  get '/:operation/:number1/:number2' do
-    operation = params[:operation]
-    if operation == "multiply"
-      answer = params[:number1].to_i * params[:number2].to_i
-    elsif operation == "add"
-      answer = params[:number1].to_i params[:number2].to_i
-    elsif operation == "subtract"
-      answer = params[:number1].to_i - params[:number2].to_i
-    else
-      answer = params[:number1].to_i / params[:number2].to_i
+   	 
+    get "/square/:number" do
+      num = params[:number].to_i
+      square = num * num
+      square.to_s
     end
-    return answer.to_s
-  end
+  
+    get "/say/:number/:phrase" do
+      answer = ''
+  
+      params[:number].to_i.times do
+        answer = params[:phrase]
+      end
+      answer
+    end
+  
+    get "/say/:word1/:word2/:word3/:word4/:word5" do
+      "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    end
+  
+    get "/:operation/:number1/:number2" do
+      number1 = params[:number1].to_i
+      number2 = params[:number2].to_i
+  
+      answer = 'Unable to perform this operation'
+  
+      case params[:operation]
+      when 'add'
+        answer = (number1  number2).to_s
+      when 'subtract'
+        answer = (number1 - number2).to_s
+      when 'multiply'
+        answer = (number1 * number2).to_s
+      when 'divide'
+        answer = (number1 / number2).to_s
+      end
+  
+    end
+  
 
 end
